8ee8f107ccd79a8eaa0d0224f827cbdc14c3af34
Jaime <jaiimecast04@gmail.com>
2024-08-23 22:16:51 -0500

    assesment1 complete

--- assessment-01/scripts/script.js
+++ assessment-01/scripts/script.js
@@ -1,3 +1,13 @@
 function scriptFunction() {
     document.getElementById("start").innerHTML="Changed by the external script";
 }
+
+function changeWidth() {
+    var image = document.getElementById("wrestler");
+    image.style.width = "500px";
+}
+
+function changeTextColor() {
+    var par = document.getElementById("start");
+    par.style.color = "blue";
+}
