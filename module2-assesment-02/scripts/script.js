7a60ab052d015e4e81a6c403b9eff9c8be752b1c
Jaime <jaiimecast04@gmail.com>
2024-09-04 22:18:24 -0500

    some progress

---
+++ module2-assesment-02/scripts/script.js
@@ -0,0 +1,23 @@
+
+// Objects
+
+// For intelligence, I did a fun ranking on what degrees these dogs would get
+const dog1 = {
+    name: "Ivy",
+    breed: "Miniature Australian Shepard",
+    size: "Small",
+    image: "pic",
+    intelligence: "Bachelors"
+};
+
+const dog2 = {
+    name: "Cookie",
+    breed: "Border Collie",
+    size: "Small",
+    image: "pic",
+    intelligence: "PhD"
+};
+
+const dog3 = {
+    
+}
