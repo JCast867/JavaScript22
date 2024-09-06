9ad81c2825ac262826d3970c7f843b740c0d884c
Jaime <jaiimecast04@gmail.com>
2024-09-05 21:36:45 -0500

    some more progress

--- module2-assesment-02/scripts/script.js
+++ module2-assesment-02/scripts/script.js
@@ -2,22 +2,55 @@
 // Objects
 
 // For intelligence, I did a fun ranking on what degrees these dogs would get
+// so not completely accurate. just my opinion :)
 const dog1 = {
     name: "Ivy",
     breed: "Miniature Australian Shepard",
     size: "Small",
-    image: "pic",
-    intelligence: "Bachelors"
+    image: "images/ivy.JPG",
+    intelligence: "Bachelors",
+    aggresiveness: "Medium"
 };
 
 const dog2 = {
     name: "Cookie",
     breed: "Border Collie",
     size: "Small",
-    image: "pic",
-    intelligence: "PhD"
+    image: "images/bordercollie.jpg",
+    intelligence: "PhD",
+    aggresiveness: "Low"
 };
 
 const dog3 = {
-    
+    name: "Sunny",
+    breed: "Golder Retriever",
+    size: "Medium",
+    image: "images/goldenretriever.jpg",
+    intelligence: "High School",
+    aggresiveness: "Low"
+};
+
+const dog4 = {
+    name: "Oreo",
+    breed: "Chihuahua",
+    size: "Extra Small",
+    image: "images/cookie.jpg",
+    intelligence: "Middle School",
+    aggresiveness: "Very High"
+};
+
+const dog5 = {
+    name: "Zeus",
+    breed: "German Shepard",
+    size: "Large",
+    image: "images/germanshepard.jpg",
+    intelligence: "Masters",
+    aggresiveness: "High"
+};
+
+function display() {
+    let html = `<h2>${dog1.name}<h2>` + `<h3>${dog1.breed}<h3>` + `<h3>${dog1.size}<h3>` + `<h3>${dog1.image}<h3>` +
+    `<h3>${dog1.intelligence}<h3>` + `<h3>${dog1.aggresiveness}<h3>`;
+
+    document.getElementById("dog1").innerHTML = html;
 }
