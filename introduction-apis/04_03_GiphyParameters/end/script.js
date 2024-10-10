b1e5111a9d87b430c77d2a4187ae70958ed28bc1
Jaime <jaiimecast04@gmail.com>
2024-10-10 16:18:43 -0500

    apis

---
+++ introduction-apis/04_03_GiphyParameters/end/script.js
@@ -0,0 +1,11 @@
+var request = new XMLHttpRequest();
+
+request.open('GET', 'https://api.giphy.com/v1/gifs/random?api_key=zmSWMv4OKkrWbtcj3GnlBwDkUJR7MVXl&tag=squirrel&rating=g');
+
+request.onload = function() {
+	var response = request.response;
+	var parsedData = JSON.parse(response);
+	console.log(parsedData);
+}
+
+request.send();
