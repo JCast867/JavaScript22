55e1d0ff07f5883ab689b23855d0a169d7a6d094
Jaime <jaiimecast04@gmail.com>
2024-10-24 14:34:17 -0500

    complete

--- learn-json/scripts/create.js
+++ learn-json/scripts/create.js
@@ -1,3 +1,11 @@
 function addme() {
+    const fname = document.getElementById("fname").value;
+    const lname = document.getElementById("lname").value;
+    const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
 
+    const obj = {"First Name": fname, "Last Name": lname, "Email": email, "Phone Number": phone};
+    const json = JSON.stringify(obj);
+
+    document.getElementById("json-value").innerHTML = json;
 }
