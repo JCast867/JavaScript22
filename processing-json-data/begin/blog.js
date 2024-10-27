61fda07060b6bc78bd621cc80dd54e7ee774225a
Jaime <jaiimecast04@gmail.com>
2024-10-26 21:22:30 -0500

    complete

--- processing-json-data/begin/blog.js
+++ processing-json-data/begin/blog.js
@@ -14,7 +14,7 @@ let posts = [
 ];
 
 // Enter code to stringify the `posts` variable here:
-
+const postsJSON = JSON.stringify(posts);
 
 let article = {
   id: "386",
@@ -24,4 +24,4 @@ let article = {
 };
 
 // Enter code to stringify the `article` variable here:
-
+const articleJSON = JSON.stringify(article);
