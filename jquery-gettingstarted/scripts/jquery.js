0b06968254ddf7d7633ad492a5725ef2ac78998e
Jaime <jaiimecast04@gmail.com>
2024-10-30 22:56:01 -0500

    complete

---
+++ jquery-gettingstarted/scripts/jquery.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function() {
+    $("#diff").click(function() {
+        $("p").hide();
+    });
+});
+
+$(document).ready(function() {
+    $("h2").click(function() {
+        $(this).hide();
+    });
+});
+
+$(document).ready(function() {
+    $("#hide").click(function() {
+        $("#jq").hide();
+    });
+});
